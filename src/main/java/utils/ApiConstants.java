ce3c65026551e02f868e726f02ca1fcb73360a4f
Kirill Ovcharov <babybabylonmain@yandex.ru>
2025-02-19 18:08:42 +0300

    add endpoints

---
+++ src/main/java/utils/ApiConstants.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class ApiConstants {
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site/api";
+    public static final String REGISTER_ENDPOINT = BASE_URL + "/auth/register";
+    public static final String LOGIN_ENDPOINT = BASE_URL + "/auth/login";
+    public static final String USER_ENDPOINT = BASE_URL + "/auth/user";
+    public static final String ORDERS_ENDPOINT = BASE_URL + "/orders";
+}
